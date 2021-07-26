7a01ede0c8b28ceb2cac104f71230fc801ecb69f
eleksir <eleksir@exs-elm.ru>
2021-07-26 20:25:47 +0300

    Используем более короткую форму для фатальных ошибок, более подробное логгирование

--- main.go
+++ main.go
@@ -15,29 +15,25 @@ import (
 func main() {
 	cfg, err := ini.Load("galichbot.ini")
 	if err != nil {
-		log.Println(err)
-		os.Exit(1)
+		log.Fatalln(err)
 	}
 
 	token := cfg.Section("").Key("BotApiKey").String()
 	victim, err := cfg.Section("").Key("VictimID").Int64()
 	if err != nil {
-		log.Println(err)
-		os.Exit(1)
+		log.Fatalln(err)
 	}
 
 	fileName := "phrases.txt"
 	fileBytes, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		log.Println(err)
-		os.Exit(1)
+		log.Fatalln(err)
 	}
 
 	speeches := strings.Split(string(fileBytes), "--")
 	bot, err := micha.NewBot(token)
 	if err != nil {
-		log.Println(err)
-		return
+		log.Fatalln(err)
 	}
 
 	go bot.Start()
@@ -45,10 +41,26 @@ func main() {
 
 	for update := range bot.Updates() {
 		if update.Message != nil {
+			log.Printf(
+					"%s %s %s (%d) from %s (%d) says: $s",
+					update.Message.From.Username,
+					update.Message.From.FirstName,
+					update.Message.From.LastName,
+					update.Message.From.ID,
+					update.Message.Chat.Title,
+					update.Message.Chat.ID,
+					update.Message.Text
+			)
+
 			if update.Message.From.ID == victim {
 				n := rand.Int() % len(speeches)
 				var options = micha.SendMessageOptions{ReplyToMessageID: update.Message.MessageID}
-				_, _ = bot.SendMessage(update.Message.Chat.ID, speeches[n], &options)
+				_, err = bot.SendMessage(update.Message.Chat.ID, speeches[n], &options)
+				if err != nil {
+					log.Println(err)
+				} else {
+					log.Printf("Bot answers: %s", speeches[n])
+				}
 			}
 		}
 	}
