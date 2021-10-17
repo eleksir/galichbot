01bb29ff21cb9d7f4d8126219aa4a9380b2cfc4b
eleksir <eleksir@exs-elm.ru>
2021-10-17 17:24:45 +0300

    Чистка кода, конфигов, док-ции, прогон lint-ера
    * Подчищены импорты
    * Исправлен формат printf
    * Исправлены концы строк с dos-формата на unix-формат
    * Задавлена часть "лишних" сообщений golangci-lint

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"io/ioutil"
 	"log"
 	"math/rand"
-	"os"
 	"strings"
 	"time"
 
@@ -14,24 +13,28 @@ import (
 
 func main() {
 	cfg, err := ini.Load("galichbot.ini")
+
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	token := cfg.Section("").Key("BotApiKey").String()
 	victim, err := cfg.Section("").Key("VictimID").Int64()
+
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	fileName := "phrases.txt"
 	fileBytes, err := ioutil.ReadFile(fileName)
+
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	speeches := strings.Split(string(fileBytes), "--")
 	bot, err := micha.NewBot(token)
+
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -42,20 +45,22 @@ func main() {
 	for update := range bot.Updates() {
 		if update.Message != nil {
 			log.Printf(
-					"%s %s %s (%d) from %s (%d) says: $s",
-					update.Message.From.Username,
-					update.Message.From.FirstName,
-					update.Message.From.LastName,
-					update.Message.From.ID,
-					update.Message.Chat.Title,
-					update.Message.Chat.ID,
-					update.Message.Text
+				"%s %s %s (%d) from %s (%s) says: %s",
+				update.Message.From.Username,
+				update.Message.From.FirstName,
+				update.Message.From.LastName,
+				update.Message.From.ID,
+				update.Message.Chat.Title,
+				update.Message.Chat.ID,
+				update.Message.Text,
 			)
 
 			if update.Message.From.ID == victim {
-				n := rand.Int() % len(speeches)
-				var options = micha.SendMessageOptions{ReplyToMessageID: update.Message.MessageID}
+				n := rand.Int() % len(speeches) //nolint:gosec
+
+				var options = micha.SendMessageOptions{ReplyToMessageID: update.Message.MessageID} //nolint:exhaustivestruct
 				_, err = bot.SendMessage(update.Message.Chat.ID, speeches[n], &options)
+
 				if err != nil {
 					log.Println(err)
 				} else {
